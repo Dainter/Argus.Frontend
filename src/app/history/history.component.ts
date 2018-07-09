dfb8c2ca295759349b1f972d7b2c0e7abb7ff175
Xiao Gang Dai <xiaogang.dai@siemens.com>
2018-07-09 11:28:24 +0800

    1. Echarts application on History component.

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -33,44 +33,136 @@ export class HistoryComponent implements OnInit {
   }
 
   TimeChartInit() {
-    this.timeData = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
+    this.timeOptions = {
+      color: ['#05AAFF', '#FF4371', '#FFD538'],
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        left: 'left',
+        data: ['VC40', 'VC50'],
+        textStyle: {
+          color: '#000000',
+        },
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          magicType: {show: true, type: ['stack', 'tiled']},
+          saveAsImage: {show: true}
+        }
+      },
+      xAxis: [
         {
-          label: "VC40",
+          type: 'category',
+          data: ["January", "February", "March", "April", "May", "June", "July"],
+          axisTick: {
+            alignWithLabel: true,
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#000000',
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+              shadowBlur: 10
+            },
+          },
+          axisLabel: {
+            textStyle: {
+              color: '#000000',
+            },
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          axisLine: {
+            lineStyle: {
+              color: '#000000',
+            },
+          },
+          axisLabel: {
+            textStyle: {
+              color: '#000000',
+            },
+          },
+        },
+      ],
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      series: [
+        {
+          name: 'VC40',
+          type: 'line',
           data: [65, 59, 80, 81, 56, 55, 40],
-          borderColor: "rgba(0, 255, 0, 1)",
-          borderWidth: 1,
-          backgroundColor: "rgba(0, 255, 0, 0.3)"
+          lineStyle: {
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
         },
         {
-          label: "VC50",
+          name: 'VC50',
+          type: 'line',
           data: [20, 10, 12, 3, 23, 5, 4],
-          borderColor: "rgba(0, 0, 255, 1)",
-          borderWidth: 1,
-          backgroundColor: "rgba(0, 0, 255, 0.3)"
-        }
-      ]
-    };
-    this.timeOptions = {
-      responsive: true
+          lineStyle: {
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+            shadowBlur: 10
+          },
+        },
+      ],
     };
   }
 
   PriorityChartInit() {
-    this.priorityData = {
-      labels: ["Info", "Warning", "Error"],
-      datasets: [
-        {
-          label: "VC40",
-          data: [123, 50, 32],
-          backgroundColor: ["rgba(5, 155, 255, 1)", "rgba(255, 194, 51, 1)", "rgba(255, 61, 103, 1)"],
-        }
-      ]
-    };
     this.priorityOptions = {
-      responsive: true,
-      animateRotate: true
+      color: ["rgba(5, 155, 255, 1)", "rgba(255, 194, 51, 1)", "rgba(255, 61, 103, 1)"],
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)',
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Info', 'Warning', 'Error'],
+      },
+      series: [
+        {
+          name: 'Priorities',
+          type: 'pie',
+          radius: '80%',
+          center: ['50%', '50%'],
+          data: [
+            { value: 123, name: 'Info' },
+            { value: 50, name: 'Warning' },
+            { value: 32, name: 'Error' },
+          ],
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: '#AAAAAA',
+            },
+          },
+          label: {
+            normal: {
+              textStyle: {
+                color: '#000000',
+              },
+            },
+          },
+          labelLine: {
+            normal: {
+              lineStyle: {
+                color: '#000000',
+              },
+            },
+          },
+        },
+      ],
     };
   }
 }
